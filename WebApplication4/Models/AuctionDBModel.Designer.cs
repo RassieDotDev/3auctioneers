fc24b09aee2ed30b337583578166f509c38fcd0f
MagicMan <36758264+AlexanderGeere@users.noreply.github.com>
2018-05-10 10:46:31 +0200

    Added pic link and time field to database

--- WebApplication4/Models/AuctionDBModel.Designer.cs
+++ WebApplication4/Models/AuctionDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Alexg_000\Documents\GitHub\3auctioneers\WebApplication4\Models\AuctionDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alexander Geere\Documents\GitHub\3auctioneers\WebApplication4\Models\AuctionDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
