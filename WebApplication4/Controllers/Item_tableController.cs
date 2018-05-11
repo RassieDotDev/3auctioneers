f0624a152ba9cfdaa2b88b1281b2db572b85f059
MagicMan <36758264+AlexanderGeere@users.noreply.github.com>
2018-05-12 00:09:29 +0200

    Pitures

--- WebApplication4/Controllers/Item_tableController.cs
+++ WebApplication4/Controllers/Item_tableController.cs
@@ -56,8 +56,7 @@ namespace WebApplication4.Models
                 if (file != null && file.ContentLength > 0)
                         try
                         {
-                            path = Path.Combine(Server.MapPath("~/Images"),
-                                                       Path.GetFileName(file.FileName));
+                            path = "\\Images\\" + Path.GetFileName(file.FileName);
                             file.SaveAs(path);
                             ViewBag.Message = "File uploaded successfully";
                         }
