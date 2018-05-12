e8157c3d7e6920d3fe2fb026884f5eec40add486
MagicMan <36758264+AlexanderGeere@users.noreply.github.com>
2018-05-12 02:46:43 +0200

    Tried my bes

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Validation;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using WebApplication4.Models;
 
@@ -17,6 +18,11 @@ namespace WebApplication4.Controllers
 
 
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
@@ -33,6 +39,52 @@ namespace WebApplication4.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,prod_name,prod_des,prod_sbid,prod_cbid,prod_pic")] Item_table item_table, HttpPostedFileBase file)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string path = "";
+                if (file != null && file.ContentLength > 0)
+                    try
+                    {
+                        path = "\\Images\\" + Path.GetFileName(file.FileName);
+                        file.SaveAs(path);
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                    }
+                else
+                {
+                    ViewBag.Message = "You have not specified a file.";
+                }
+                item_table.prod_pic = path;
+                db.Item_table.Add(item_table);
+                db.SaveChanges();
+                return RedirectToAction("Auction", "Home");
+
+            }
+            return View(item_table);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,prod_name,prod_des,prod_sbid,prod_cbid")] Item_table item_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item_table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item_table);
+            
         }
 
         public ActionResult Auction(int? id)
