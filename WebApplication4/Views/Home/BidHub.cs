bf6dcbab81432ad6b1f1013914e7ec733ccc1e10
ThABlEh <36758272+ThABlEh@users.noreply.github.com>
2018-05-15 17:45:40 +0200

    zx
    zx

---
+++ WebApplication4/Views/Home/BidHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using WebApplication4.Models;
+
+namespace WebApplication4
+{
+    [HubName("LiveBid")]
+    public class BidHub : Hub
+    {
+        
+        private static double bidprice = 0;
+        public void AddBid(Item_table newbid)
+        {
+            bidprice = newbid.prod_cbid;     
+
+            this.Clients.All.onBidRecorded(bidprice);
+        }
+        
+    }
+}
